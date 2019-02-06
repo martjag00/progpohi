4504ddf48826eb67670d7c6a769c38a57340daa6
martjag00 <martjag00@users.norey-github.com>
2019-02-06 13:03:15 +0200

    teine test

--- Arvutus.java
+++ Arvutus.java
@@ -1,8 +1,17 @@
-public class Tervitus{
+public class Arvutus{
 	public static void main(String[] arg){
 		int vanus=12;
 		if(vanus<18){
 			System.out.println("lapsepilet");
+		} else {
+			if(vanus<7){
+				System.out.println("Tasuta");
+			} else {
+				System.out.println("Täispilet");
+			}
+		}	
+		for(int nr=0; nr<vanus; nr++){
+			System.out.println("Õnne!");
 		}
 	}
 }
